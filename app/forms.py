31203ee541a8e18df2c9b9088bec501ec44cd16b
XFishalways <xfishtnt@gmail.com>
2023-11-07 16:14:11 +0800

    templated edited

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, Email, DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name")
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
---
+++ run.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+
+from app import app, db
+
+db.create_all()
